0e2846864b67d74ca7864c933a8bd2d1e8dca32a
zhangzhandong <zzd@choicesoft.com.cn>
2018-04-04 14:25:40 +0800

    numberDiv1 完成

--- JSdemo/js/numberDiv1.js
+++ JSdemo/js/numberDiv1.js
@@ -1,7 +1,25 @@
- function start(){
-    document.getElementByName("number").addEventListener("click",()=>{
-      let num=getElementById
-    }
-    ,false)
- }
- window.addEventListener("load", start, false);
+function start() {
+  document.getElementById("button1").addEventListener("click", () => {
+    clear();
+    document.getElementById("number1").innerHTML = 1;
+  }, false);
+  document.getElementById("button2").addEventListener("click", () => {
+    clear();
+    document.getElementById("number2").innerHTML = 2;
+  }, false);
+  document.getElementById("button3").addEventListener("click", () => {
+    clear();
+    document.getElementById("number3").innerHTML = 3;
+  }, false);
+  document.getElementById("button4").addEventListener("click", () => {
+    clear();
+    document.getElementById("number4").innerHTML = 4;
+  }, false);
+}
+function clear() {
+  document.getElementById("number1").innerHTML = "";
+  document.getElementById("number2").innerHTML = "";
+  document.getElementById("number3").innerHTML = "";
+  document.getElementById("number4").innerHTML = "";
+}
+window.addEventListener("load", start, false);
