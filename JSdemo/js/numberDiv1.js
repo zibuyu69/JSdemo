7de2ed312be06f803fd3cdce8c0303238e69be80
zibuyu69 <843845614@qq.com>
2018-04-04 13:54:26 +0800

    numberDiv1
    李

---
+++ JSdemo/js/numberDiv1.js
@@ -0,0 +1,7 @@
+ function start(){
+    document.getElementByName("number").addEventListener("click",()=>{
+      let num=getElementById
+    }
+    ,false)
+ }
+ window.addEventListener("load", start, false);
