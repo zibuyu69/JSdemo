681b21eb22b92c383fc56a712178d069b30ffe3d
zibuyu69 <843845614@qq.com>
2018-04-04 16:16:54 +0800

    div4

--- JSdemo/js/numberDiv3.js
+++ JSdemo/js/numberDiv3.js
@@ -1,6 +1,11 @@
 function start() {
+  let sum=1;
   document.getElementById("button").addEventListener("click", () => {
-    document.getElementById("div").innerHTML += '<div id="number1" style="width:100px;height:100px;display:inline-block;background:#d3f261"></div>';
+
+document.getElementById("div").innerHTML += '<div id="number1" style="width:100px;height:100px;display:inline-block;background:#d3f261"> 第'+sum+'个</div>';
+sum ++
+
+
   }, false)
 }
 window.addEventListener("load", start, false);
---
+++ JSdemo/js/numberDiv4.js
@@ -0,0 +1,10 @@
+function  start(){
+  let arr=[2,4,6,9,10]
+  document.getElementById("button").addEventListener("click", () => {
+  for(let i=0;i<arr.length;i++){
+    document.getElementById("div").innerHTML+= '<div id="number1" style="width:100px;height:100px;display:inline-block;background:#d3f261">'+ arr[i]+'</div>';
+  }
+    }, false)
+
+}
+window.addEventListener("load", start, false);
