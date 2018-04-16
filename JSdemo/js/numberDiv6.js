47a33e973d3527bfc6bf33fa3cb2e3f742f3824e
zibuyu69 <843845614@qq.com>
2018-04-16 17:25:07 +0800

    6.2

--- JSdemo/js/numberDiv6.js
+++ JSdemo/js/numberDiv6.js
@@ -1,24 +1,15 @@
 function start(){
 let text=document.getElementById("text2").value
 console.log(text);
-if(text>=1 && text<=3){
  let number=text;
- for(let i=1;i<=3;i++){
-    if(number==i){
-      let divx=document.getElementById("div"+i);
+     let divx=document.getElementById("div"+number);
       console.log(divx);
       let style =divx.getAttribute("style");
       console.log(style);
       divx.setAttribute("style","width:100px;height: 100px;background:#610b00")
-    }
- }
-}else{
-  let otest=document.getElementById("div");
-  let oldDiv=document.getElementById("text2");
-  let newDiv=document.createElement("p");
-  newDiv.innerHTML="非法字符";
-  otest.insertBefore(newDiv,oldDiv);
-}
+
+
+
 }
 function change(){
   let num=document.getElementById("text1").value;
@@ -26,11 +17,9 @@ function change(){
   for(let i=1;i<=num;i++){
     let otest=document.getElementById("div");
     let newdiv=document.createElement("div");
-    newdiv.innerHTML="001";
+    newdiv.innerHTML="00"+i;
     otest.appendChild(newdiv);
     newdiv.setAttribute("id","div"+i);
     newdiv.setAttribute("style","width:100px;height: 100px;background:#d3f261")
-
-
   }
 }
