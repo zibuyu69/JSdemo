f2365f56afdf4d89f85d27768c4f7ec42fccdfc6
zibuyu69 <843845614@qq.com>
2018-08-22 18:08:14 +0800

    01

--- JSdemo/js/text.js
+++ JSdemo/js/text.js
@@ -1,7 +1,33 @@
-function outer(){
-  var i=1;
-  return i;
-  console.log(i);
-  alert(001);
+function init() {
+  document.getElementById("tb1").addEventListener(
+    "mouseenter",
+    () => {
+      if (document.getElementById("tb1").className === "button") {
+        document.getElementById("tb1").className = "button active";
+        document.getElementById("tb2").className = "button";
+      }
+      if (document.getElementById("tab1").className === "context hide") {
+        document.getElementById("tab1").className = "context";
+        document.getElementById("tab2").className = "context hide";
+      }
+    },
+    false
+  );
+  document.getElementById("tb2").addEventListener(
+    "mouseenter",
+    () => {
+      if (document.getElementById("tb2").className === "button") {
+        document.getElementById("tb2").className = "button active";
+        document.getElementById("tb1").className = "button";
+      }
+
+      if (document.getElementById("tab2").className === "context hide") {
+        document.getElementById("tab2").className = "context";
+        document.getElementById("tab1").className = "context hide";
+      }
+    },
+    false
+  );
 }
-  outer();
+// 当网页开始load时候运行init 函数
+window.addEventListener("load", init, false);
